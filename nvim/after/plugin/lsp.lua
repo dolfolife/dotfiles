6b8e60c4028ab06b2a3737c77be9c5cd75ecc2e7
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-04-17 15:32:00 -0500

    add lspconfig
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -28,6 +28,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
 lsp.setup()
 
 vim.diagnostic.config({
