8ed3f735a97de1558d8d756a4314539d14f357b2
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-04-17 13:44:08 -0500

    add lsp I use

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,4 +1,35 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'gopls',
+    'kotlin_language_server',
+    'lua_ls',
+    'hls',
+    'jsonls',
+    'eslint',
+    'tsserver'
+})
+
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+  vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+  vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
 lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true
+})
