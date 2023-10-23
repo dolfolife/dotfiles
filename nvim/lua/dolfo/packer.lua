f54ce87fb712d0d0cb6491f75860d6b0ec6e1d57
Rodolfo Sanchez <rodolfo2488@gmail.com>
2023-10-23 13:14:30 -0500

    use branch instead of a tag
    Signed-off-by: Rodolfo Sanchez <rodolfo2488@gmail.com>

--- nvim/lua/dolfo/packer.lua
+++ nvim/lua/dolfo/packer.lua
@@ -5,8 +5,7 @@ return require('packer').startup(function(use)
     use('wbthomason/packer.nvim')
 
     use({
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
+        'nvim-telescope/telescope.nvim', branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     })
     use({
